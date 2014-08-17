ffc5266c29cf31b3c7c7e583363656626806c70d
Jaguar0625 <jaguar0625@127.0.0.1>
2014-08-17 17:37:21 -0400

    fix handling of CompletionException

--- nem-client-api/src/main/java/org/nem/deploy/NccWebAppInitializer.java
+++ nem-client-api/src/main/java/org/nem/deploy/NccWebAppInitializer.java
@@ -6,9 +6,13 @@ import org.nem.ncc.controller.interceptors.AuditInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.*;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
+import javax.servlet.http.*;
 import java.util.List;
+import java.util.concurrent.CompletionException;
 
 /**
  * Class supplying Spring MVC configuration.
@@ -40,5 +44,28 @@ public class NccWebAppInitializer extends WebMvcConfigurationSupport {
 	protected void addInterceptors(final InterceptorRegistry registry) {
 		registry.addInterceptor(new AuditInterceptor());
 		super.addInterceptors(registry);
+	}
+
+	@Override
+	protected void configureHandlerExceptionResolvers(final List<HandlerExceptionResolver> exceptionResolvers) {
+		exceptionResolvers.add(new CompletionExceptionResolver());
+	}
+
+	/**
+	 * A custom HandlerExceptionResolver that automatically unwraps CompletionException exceptions.
+	 * Spring is then able to dispatch the unwrapped exception to @ExceptionHandler methods.
+	 */
+	private static class CompletionExceptionResolver extends ExceptionHandlerExceptionResolver {
+
+		@Override
+		public ModelAndView resolveException(final HttpServletRequest request, final HttpServletResponse response, final Object handler, final Exception ex) {
+			return super.resolveException(request, response, handler, unwrap(ex));
+		}
+
+		private static Exception unwrap(final Exception ex) {
+			return ex instanceof CompletionException && ex.getCause() instanceof Exception
+					? (Exception)ex.getCause()
+					: ex;
+		}
 	}
 }
--- nem-client-api/src/main/java/org/nem/ncc/controller/ExceptionControllerAdvice.java
+++ nem-client-api/src/main/java/org/nem/ncc/controller/ExceptionControllerAdvice.java
@@ -75,23 +75,6 @@ public class ExceptionControllerAdvice {
 	}
 
 	/**
-	 * Handler for future completion exceptions.
-	 *
-	 * @param e The exception.
-	 * @return The appropriate JSON indicating an error.
-	 */
-	@ExceptionHandler(CompletionException.class)
-	public ResponseEntity<ErrorResponse> handleCompletionException(final CompletionException e) {
-		// TODO-CR: 20140817 J->T i think it would be more appropriate to fix this in an interceptor if possible
-		Throwable ex = e.getCause();
-		if (ex instanceof NisException) {
-			return this.handleNccException((NisException)ex);
-		}
-
-		return this.createResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
-	/**
 	 * Handler for general exceptions.
 	 *
 	 * @param e The exception.
