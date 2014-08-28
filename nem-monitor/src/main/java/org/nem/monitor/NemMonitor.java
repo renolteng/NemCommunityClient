7a22d63cc2b6bc9406ee8f5aaf0ee1a062a144a9
Jaguar0625 <jaguar0625@127.0.0.1>
2014-08-27 23:25:42 -0400

    allow only one monitor instance to run at a time

--- nem-monitor/src/main/java/org/nem/monitor/NemMonitor.java
+++ nem-monitor/src/main/java/org/nem/monitor/NemMonitor.java
@@ -2,12 +2,15 @@ package org.nem.monitor;
 
 import org.nem.core.connect.*;
 import org.nem.core.deploy.LoggingBootstrapper;
+import org.nem.core.utils.LockFile;
 import org.nem.monitor.config.*;
 import org.nem.monitor.node.*;
 import org.nem.monitor.ux.TrayIconBuilder;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 /**
@@ -15,6 +18,7 @@ import java.util.logging.Logger;
  */
 public class NemMonitor {
 	private static final Logger LOGGER = Logger.getLogger(NemMonitor.class.getName());
+	private static Closeable fileLockHandle;
 
 	static {
 		// initialize logging before anything is logged; otherwise not all
@@ -32,6 +36,11 @@ public class NemMonitor {
 		LOGGER.info(String.format("NCC JNLP configured as: %s", commandLine.getNccJnlpUrl()));
 		LOGGER.info(String.format("NIS JNLP configured as: %s", commandLine.getNisJnlpUrl()));
 
+		final String nemFolder = new MonitorConfiguration().getNemFolder();
+		if (!canStart(nemFolder)) {
+			return;
+		}
+
 		SwingUtilities.invokeLater(() -> {
 			LOGGER.info("setting up system tray");
 
@@ -40,7 +49,6 @@ public class NemMonitor {
 				throw new SystemTrayException("SystemTray is not supported");
 			}
 
-			final String nemFolder = new MonitorConfiguration().getNemFolder();
 			final SystemTray tray = SystemTray.getSystemTray();
 			final TrayIconBuilder builder = new TrayIconBuilder(
 					createHttpMethodClient(),
@@ -66,5 +74,17 @@ public class NemMonitor {
 		final int SOCKET_TIMEOUT = 2000;
 		final int REQUEST_TIMEOUT = 4000;
 		return new HttpMethodClient<>(CONNECTION_TIMEOUT, SOCKET_TIMEOUT, REQUEST_TIMEOUT);
+	}
+
+	private static boolean canStart(final String nemFolder) {
+		final File lockFile = Paths.get(nemFolder, "mon.lock").toFile();
+		LOGGER.info(String.format("Acquiring exclusive lock to lock file: %s", lockFile));
+		fileLockHandle = LockFile.tryAcquireLock(lockFile);
+		if (null == fileLockHandle) {
+			LOGGER.severe(String.format("Could not acquire exclusive lock to lock file"));
+			return false;
+		}
+
+		return true;
 	}
 }
