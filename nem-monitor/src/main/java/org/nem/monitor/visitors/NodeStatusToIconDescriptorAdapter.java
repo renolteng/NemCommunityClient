b467eedb9ff3c44c97186432122a669a3f831b38
BloodyRookie <nemproject@gmx.de>
2014-09-08 11:52:49 +0200

    renamed icons, automated launch NIS/NCC when started via web start.

--- nem-monitor/src/main/java/org/nem/monitor/visitors/NodeStatusToIconDescriptorAdapter.java
+++ nem-monitor/src/main/java/org/nem/monitor/visitors/NodeStatusToIconDescriptorAdapter.java
@@ -12,46 +12,46 @@ import java.util.function.*;
 public class NodeStatusToIconDescriptorAdapter implements NodeStatusVisitor {
 	private static final List<BiFunction<NemStatus, NemStatus, IconDescriptor>> iconDescriptorPredicates = Arrays.asList(
 			(nccStatus, nisStatus) -> (NemStatus.STARTING == nisStatus && NemStatus.STOPPED == nccStatus)
-					? new IconDescriptor("nis_starting_ncc_stopped.png", "NIS is booting, NCC is stopped")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is booting, NCC is stopped")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.RUNNING == nisStatus && NemStatus.STOPPED == nccStatus)
-					? new IconDescriptor("nis_running_ncc_stopped.png", "NIS is running (local node not booted), NCC is stopped")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running (local node not booted), NCC is stopped")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.BOOTED == nisStatus && NemStatus.STOPPED == nccStatus)
-					? new IconDescriptor("nis_running_node_booted_ncc_stopped.png", "NIS is running (local node booted), NCC is stopped")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running (local node booted), NCC is stopped")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.SYNCHRONIZED == nisStatus && NemStatus.STOPPED == nccStatus)
-					? new IconDescriptor("nis_running_node_synchronized_ncc_stopped.png", "NIS is running and is synchronized, NCC is stopped")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running and is synchronized, NCC is stopped")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.STOPPED == nisStatus && NemStatus.STARTING == nccStatus)
-					? new IconDescriptor("nis_stopped_ncc_starting.png", "NIS is stopped, NCC is starting")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is stopped, NCC is starting")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.STARTING == nisStatus && NemStatus.STARTING == nccStatus)
-					? new IconDescriptor("nis_starting_ncc_starting.png", "NIS is starting, NCC is starting")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is starting, NCC is starting")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.RUNNING == nisStatus && NemStatus.STARTING == nccStatus)
-					? new IconDescriptor("nis_running_ncc_starting.png", "NIS is running (local node not booted), NCC is booting")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running (local node not booted), NCC is booting")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.BOOTED == nisStatus && NemStatus.STARTING == nccStatus)
-					? new IconDescriptor("nis_running_node_booted_ncc_starting.png", "NIS is running (local node booted), NCC is starting")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running (local node booted), NCC is starting")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.SYNCHRONIZED == nisStatus && NemStatus.STARTING == nccStatus)
-					? new IconDescriptor("nis_running_node_synchronized_ncc_starting.png", "NIS is running and is synchronized, NCC is starting")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running and is synchronized, NCC is starting")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.STOPPED == nisStatus && NemStatus.RUNNING == nccStatus)
-					? new IconDescriptor("nis_stopped_ncc_running.png", "NIS is stopped, NCC is running")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is stopped, NCC is running")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.STARTING == nisStatus && NemStatus.RUNNING == nccStatus)
-					? new IconDescriptor("nis_starting_ncc_running.png", "NIS is starting, NCC is running")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is starting, NCC is running")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.RUNNING == nisStatus && NemStatus.RUNNING == nccStatus)
-					? new IconDescriptor("nis_running_ncc_running.png", "NIS is running (local node not booted), NCC is running")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running (local node not booted), NCC is running")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.BOOTED == nisStatus && NemStatus.RUNNING == nccStatus)
-					? new IconDescriptor("nis_running_node_booted_ncc_running.png", "NIS is running (local node booted), NCC is running")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running (local node booted), NCC is running")
 					: null,
 			(nccStatus, nisStatus) -> (NemStatus.SYNCHRONIZED == nisStatus && NemStatus.RUNNING == nccStatus)
-					? new IconDescriptor("nis_running_node_synchronized_ncc_running.png", "NIS is running and is synchronized, NCC is running")
+					? new IconDescriptor(getImageName(nccStatus, nisStatus), "NIS is running and is synchronized, NCC is running")
 					: null);
 
 	private final Consumer<IconDescriptor> iconDescriptorConsumer;
@@ -92,6 +92,10 @@ public class NodeStatusToIconDescriptorAdapter implements NodeStatusVisitor {
 			}
 		}
 
-		return new IconDescriptor("all_bad.png", "Neither NCC nor NIS is running");
+		return new IconDescriptor("icon_00.png", "Neither NCC nor NIS is running");
+	}
+
+	private static String getImageName(final NemStatus nccStatus, final NemStatus nisStatus) {
+		return String.format("icon_%d%d.png", nccStatus.getValue(), nisStatus.getValue());
 	}
 }
