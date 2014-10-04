58bc9ad0362527059931759c7be59e80ee95617e
gimre <gimre@127.0.0.1>
2014-10-04 14:16:28 +0200

    update bump script

--- bumpversion.bat
+++ bumpversion.bat
@@ -21,6 +21,12 @@ pushd nem-client-api
   rm pom.out
 
   git add pom.xml
-  sed -n "/ALPHA/{ s/.*\([0-9]\+.[0-9]\+.[0-9]\+-ALPHA\).*/bump version to \1/; p }" pom.xml | xargs -iXX git commit -m XX
 popd
+
+awk "{ if (match($0, /(.*\.)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < .travis.yml > .travis.out
+tr --delete "\r" < .travis.out > .travis.yml
+rm .travis.out
+
+git add .travis.yml
+sed -n "/ALPHA/{ s/.*\([0-9]\+.[0-9]\+.[0-9]\+-ALPHA\).*/bump version to \1/; p }" .travis.yml | xargs -iXX git commit -m XX
 
