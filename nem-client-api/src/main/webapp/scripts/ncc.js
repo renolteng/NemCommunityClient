de65af7dcb0d124c2bce14510f7f375931ccc7ea
Krysto <quy.p.hoang@gmail.com>
2014-10-24 01:40:42 +0700

    TX details modal shows real amounts

--- nem-client-api/src/main/webapp/scripts/ncc.js
+++ nem-client-api/src/main/webapp/scripts/ncc.js
@@ -904,6 +904,8 @@ define(function(require) {
             tx.formattedRecipient = this.formatAddress(tx.recipient);
             tx.formattedFee = this.formatCurrency(tx.fee, true);
             tx.formattedAmount = this.formatCurrency(tx.amount, true);
+            tx.formattedFullFee = this.formatCurrency(tx.fee, false, true, true);
+            tx.formattedFullAmount = this.formatCurrency(tx.amount, false, true, true);
             tx.formattedDate = this.formatDate(tx.timeStamp, 'M dd, yyyy hh:mm:ss');
             return tx;
         },
