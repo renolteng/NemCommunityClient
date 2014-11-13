a71f79d8afc43711961b47dd3d50cd43d9cbd0a0
gimre <gimre@127.0.0.1>
2014-11-13 20:03:22 +0100

    slight changes in build scripts (ncc part)

--- nem-client-api/building/buildPackage-ws-ncc.sh
+++ nem-client-api/building/buildPackage-ws-ncc.sh
@@ -1,18 +1,13 @@
-cd ../../../nis
-rm site/serverjars/nem-infra*
-rm webstart/html/lib/nem-infra*
-mvn -fpom-core.xml clean compile install -Duser.name=$3 -DskipTests=true
-
-cd ../ncc/nem-client-download
+cd ../../../ncc/nem-client-download
 mvn clean compile install -Duser.name=$3 -DskipTests=true  
-
-cd ../nem-monitor
-mvn clean compile install -DskipTests=true -Dnem-certificate-store=$1 -Dnem-certificate-password=$2
 
 cd ../nem-client-api
 rm target/nem-*
 rm site/jars/nem-*
 mvn clean compile install -Duser.name=$3 -DproGuard=$4 -DskipTests=true -Dnem-certificate-store=$1 -Dnem-certificate-password=$2
+
+cd ../nem-monitor
+mvn clean compile install -DskipTests=true -Dnem-certificate-store=$1 -Dnem-certificate-password=$2
 
 cd ../nem-client-api/building
 rm nem-*
