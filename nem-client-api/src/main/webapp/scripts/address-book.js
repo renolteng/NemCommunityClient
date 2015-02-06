274b886f0bc4a6bd0ed63865995f0d3572fbd292
BloodyRookie <nemproject@gmx.de>
2015-02-06 21:42:35 +0100

    asc/desc sorting of address book.

--- nem-client-api/src/main/webapp/scripts/address-book.js
+++ nem-client-api/src/main/webapp/scripts/address-book.js
@@ -15,16 +15,18 @@ define(['jquery', 'ncc', 'NccLayout', 'Utils', 'ractive-events-tap'], function($
                 console.log( 'Will sort by ' + column );
             });
 
+            ncc.set('contactsSortOrder', { 'formattedAddress': -1, 'privateLabel': -1 });
             ncc.set('sortContacts',
                 function (array, sortColumn) {
                     array = array.slice(); // clone, so we don't modify the underlying data
+                    ncc.get('contactsSortOrder')[sortColumn] = -ncc.get('contactsSortOrder')[sortColumn];
 
                     return array.sort(function(a, b) {
-                        return a[sortColumn] < b[sortColumn] ? -1 : 1;
+                        return ncc.get('contactsSortOrder')[sortColumn] * (a[sortColumn] < b[sortColumn] ? -1 : 1);
                     });
                 }
             );
-        },
+         },
         setupEverytime: function() {
         },
         leave: [function() {}]
