52186986f08d8b9c1508f807c5ec0d1f924d1414
gimre <gimre@127.0.0.1>
2015-03-29 21:03:08 +0200

    alter building a bit

--- nem-client-api/building/buildPackage-ws-ncc.sh
+++ nem-client-api/building/buildPackage-ws-ncc.sh
@@ -1,16 +1,13 @@
 cd ../../../ncc/nem-client-api
 rm target/nem-*
 rm site/jars/nem-*
-mvn clean compile install -Duser.name=$3 -DproGuard=$4 -DskipTests=true -Dnem-certificate-store=$1 -Dnem-certificate-password=$2
+mvn clean compile install -Duser.name=$3 -DproGuard=$4 -DskipTests=true -Dmaven.test.skip=true -Dnem-certificate-store=$1 -Dnem-certificate-password=$2
 
 cd ../nem-monitor
-mvn clean compile install -DskipTests=true -Dnem-certificate-store=$1 -Dnem-certificate-password=$2
+mvn clean compile install -DskipTests=true -Dmaven.test.skip=true -Dnem-certificate-store=$1 -Dnem-certificate-password=$2
 
 cd ../nem-client-api/building
 rm nem-*
 bash ./copy-ws.sh
 bash ./buildPackage-ws-sign-ncc.sh $1 $2 $3
 cd ../site
-rm NCC.zip
-7z a -r -x!NCC.zip NCC.zip *
-cp NCC.zip ../../..
