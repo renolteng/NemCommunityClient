44124fdf91ace0f1647fa809862b12a471e321e7
gimre <gimre@127.0.0.1>
2015-03-29 17:29:38 +0200

    bump version to 0.6.25

--- bumpversion.bat
+++ bumpversion.bat
@@ -8,6 +8,15 @@ pushd nem-monitor
   git add pom.xml
 popd
 
+pushd nem-console
+  awk "{ if (match($0, /(.*\.)([0-9]+)(-BETA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom.xml > pom.out
+  tr --delete "\r" < pom.out > pom.xml
+  rm pom.out
+
+  git add pom.xml
+popd
+
+
 pushd nem-client-api
   awk "{ if (match($0, /(.*\.)([0-9]+)(-BETA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom.xml > pom.out
   tr --delete "\r" < pom.out > pom.xml
--- install-core.bat
+++ install-core.bat
@@ -1,2 +1,2 @@
-wget http://bob.nem.ninja/nem-core__V0.4.42-BETA.jar
-mvn install:install-file -Dfile=nem-core__V0.4.42-BETA.jar -DgroupId=org.nem.core -DartifactId=nem-core -Dversion=0.4.42-BETA -Dpackaging=jar
+wget http://bob.nem.ninja/nem-core__V0.6.25-BETA.jar
+mvn install:install-file -Dfile=nem-core__V0.6.25-BETA.jar -DgroupId=org.nem.core -DartifactId=nem-core -Dversion=0.6.25-BETA -Dpackaging=jar
