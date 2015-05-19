e2e5a375ad2f132127495b412c04c52bcc923878
gimre <gimre@127.0.0.1>
2015-05-20 00:21:12 +0200

    announceTransaction was not using NemAnnounceResult at all...

--- nem-client-api/src/main/java/org/nem/ncc/controller/TransactionController.java
+++ nem-client-api/src/main/java/org/nem/ncc/controller/TransactionController.java
@@ -42,9 +42,9 @@ public class TransactionController {
 	 * @param transferRequest The transaction information.
 	 */
 	@RequestMapping(value = "/wallet/account/transaction/send", method = RequestMethod.POST)
-	public void sendTransaction(@RequestBody final TransferSendRequest transferRequest) {
+	public NemAnnounceResult sendTransaction(@RequestBody final TransferSendRequest transferRequest) {
 		final Transaction transaction = this.transactionMapper.toModel(transferRequest);
-		this.announceTransaction(transaction);
+		return this.announceTransaction(transaction);
 	}
 
 	/**
@@ -53,9 +53,9 @@ public class TransactionController {
 	 * @param multisigSignatureRequest The multisig signature transaction information.
 	 */
 	@RequestMapping(value = "/wallet/account/signature/send", method = RequestMethod.POST)
-	public void sendSignature(@RequestBody final MultisigSignatureRequest multisigSignatureRequest) {
+	public NemAnnounceResult sendSignature(@RequestBody final MultisigSignatureRequest multisigSignatureRequest) {
 		final Transaction transaction = this.transactionMapper.toModel(multisigSignatureRequest);
-		this.announceTransaction(transaction);
+		return this.announceTransaction(transaction);
 	}
 
 	/**
@@ -64,9 +64,9 @@ public class TransactionController {
 	 * @param multisigModification The multisig account modification transaction information.
 	 */
 	@RequestMapping(value = "/wallet/account/modification/send", method = RequestMethod.POST)
-	public void sendModification(@RequestBody final MultisigModificationRequest multisigModification) {
+	public NemAnnounceResult sendModification(@RequestBody final MultisigModificationRequest multisigModification) {
 		final Transaction transaction = this.transactionMapper.toModel(multisigModification);
-		this.announceTransaction(transaction);
+		return this.announceTransaction(transaction);
 	}
 
 	/**
@@ -135,7 +135,7 @@ public class TransactionController {
 		this.announceTransaction(transaction);
 	}
 
-	private void announceTransaction(final Transaction transaction) {
+	private NemAnnounceResult announceTransaction(final Transaction transaction) {
 		// prepare transaction
 		final byte[] transferBytes = BinarySerializer.serializeToBytes(transaction.asNonVerifiable());
 		final RequestPrepare preparedTransaction = new RequestPrepare(transferBytes);
@@ -145,11 +145,12 @@ public class TransactionController {
 		final RequestAnnounce announce = new RequestAnnounce(
 				preparedTransaction.getData(),
 				signer.sign(preparedTransaction.getData()).getBytes());
-		final NemRequestResult result = new NemRequestResult(this.nisConnector.post(
+		final NemAnnounceResult result = new NemAnnounceResult(this.nisConnector.post(
 				NisApiId.NIS_REST_TRANSACTION_ANNOUNCE,
 				new HttpJsonPostRequest(announce)));
 		if (result.isError()) {
 			throw new NisException(result);
 		}
+		return result;
 	}
 }
