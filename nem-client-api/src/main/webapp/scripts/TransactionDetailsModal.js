7de8e2cc9cfb17bd6bb598bb66c9bbf8a5a8b1b0
gimre <gimre@127.0.0.1>
2015-06-22 01:08:02 +0200

    a (non-working) attempt to separate it, will investigate

--- nem-client-api/src/main/webapp/scripts/TransactionDetailsModal.js
+++ nem-client-api/src/main/webapp/scripts/TransactionDetailsModal.js
@@ -1,11 +1,17 @@
 "use strict";
 
-define(['NccModal', 'Utils'], function(NccModal, Utils) {
+define(['jquery', 'NccModal', 'Utils'], function($, NccModal, Utils) {
 	return NccModal.extend({
-		onrender: function() {
-		    this.set('privateLabels', ncc.get('privateLabels'));
+	    onrender: function() {
+            this.set('privateLabels', ncc.get('privateLabels'));
 
             this.viewAccount = ncc.viewAccount;
+
+            // rather ugly way to make partials visible
+            this.partials['transferTransactionPartial'] = $('transferTransactionPartial').html();
+            this.partials['importanceTransactionPartial'] = $('importanceTransactionPartial').html();
+            this.partials['aggregateModificationPartial'] = $('aggregateModificationPartial').html();
+
 		}
 	});
 });
