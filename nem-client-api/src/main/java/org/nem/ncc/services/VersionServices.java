0672349574c292eb19ddfa041b4f5a5196533a71
gimre <gimre@127.0.0.1>
2015-07-29 19:53:28 +0200

    fix UI-only bug in deactivation of delegated harvesting for multisig accounts

---
+++ nem-client-api/src/main/java/org/nem/ncc/services/VersionServices.java
@@ -0,0 +1,33 @@
+package org.nem.ncc.services;
+
+import org.nem.core.time.TimeInstant;
+import org.nem.core.time.TimeProvider;
+import org.nem.ncc.connector.VersionProvider;
+import org.nem.ncc.controller.viewmodels.VersionInformationViewModel;
+
+public class VersionServices {
+	final VersionProvider versionProvider;
+	final TimeProvider timeProvider;
+	final int REFRESH_IN_SECOND = 5*60;
+	TimeInstant refreshTime;
+	VersionInformationViewModel versionInformationViewModel;
+
+	public VersionServices(final VersionProvider versionProvider, final TimeProvider timeProvider)
+	{
+		this.versionProvider = versionProvider;
+		this.timeProvider = timeProvider;
+
+		this.refreshTime = TimeInstant.ZERO;
+	}
+
+	public VersionInformationViewModel getVersions() {
+		final TimeInstant currentTime = this.timeProvider.getCurrentTime();
+		if (currentTime.subtract(refreshTime) > this.REFRESH_IN_SECOND) {
+			versionInformationViewModel = new VersionInformationViewModel(
+					this.versionProvider.getLocalVersion(),
+					this.versionProvider.getLatestVersion()
+			);
+		}
+		return versionInformationViewModel;
+	}
+}
